898b4cb6a42d65d6e28635321bde6e9a8264f0da
heerou <jalvarezpimiento@gmail.com>
2023-03-27 21:54:46 -0500

    Player has idle and running animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,12 +6,17 @@ public class PlayerController : MonoBehaviour
 {
     public float MovementSpeed;
     public float JumpForce;
-    public float GravityScale = 5f;
+    public float GravityScale;
+    public float RotationSpeed;
 
     private Vector3 moveDirection;
     public CharacterController CharContr;
 
     private Camera TheCamera;
+
+    public GameObject PlayerModel;
+
+    public Animator TheAnimator;
 
     // Start is called before the first frame update
     void Start()
@@ -30,6 +35,7 @@ public class PlayerController : MonoBehaviour
         float yStore = moveDirection.y;
         //moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
         moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right  * Input.GetAxisRaw("Horizontal"));
+        moveDirection.Normalize();
         moveDirection *= MovementSpeed;
         moveDirection.y = yStore;
 
@@ -44,6 +50,10 @@ public class PlayerController : MonoBehaviour
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
             transform.rotation = Quaternion.Euler(0f, TheCamera.transform.rotation.eulerAngles.y, 0f);
+            Quaternion newRot = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            PlayerModel.transform.rotation = Quaternion.Slerp(PlayerModel.transform.rotation, newRot, RotationSpeed * Time.deltaTime);
         }
+
+        TheAnimator.SetFloat("Speed", Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
     }
 }
