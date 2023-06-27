5bcd3ec56fe25504e6b27597ef75a7b7a5c6a590
JulioAp <jalvarezpimiento@gmail.com>
2023-06-26 20:25:41 -0500

    Added invicibility frames and the playes flashes when getting hurt

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -7,16 +7,43 @@ public class HealthManager : MonoBehaviour
 {
     public static HealthManager HealthInstance;
     public int CurrentHealth, MaxHealth;
+    public float InvincibleFramesLenght;
+    private float invicibleCounter;
 
     private void Awake()
     {
         HealthInstance = this;
     }
-    
+
     // Start is called before the first frame update
     private void Start()
     {
         CurrentHealth = MaxHealth;
+    }
+
+    private void Update()
+    {
+        if (invicibleCounter > 0)
+        {
+            invicibleCounter -= Time.deltaTime;
+            
+            for (int i = 0; i < PlayerController.Instance.PlayerPieces.Length; i++)
+            {
+                if (Mathf.Floor(invicibleCounter * 5f) % 2 == 0)
+                {
+                    PlayerController.Instance.PlayerPieces[i].SetActive(true);
+                }
+                else
+                {
+                    PlayerController.Instance.PlayerPieces[i].SetActive(false);
+                }
+
+                if (invicibleCounter <= 0)
+                {
+                    PlayerController.Instance.PlayerPieces[i].SetActive(true);
+                }
+            }
+        }
     }
 
     public void Hurt()
@@ -26,6 +53,11 @@ public class HealthManager : MonoBehaviour
         {
             CurrentHealth = 0;
             GameManager.InstanceGM.Respawn();
+        }
+        else
+        {
+            PlayerController.Instance.Knockback();
+            invicibleCounter = InvincibleFramesLenght;
         }
     }
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,6 +21,13 @@ public class PlayerController : MonoBehaviour
 
     public Animator TheAnimator;
 
+    private bool isKnockedOut;
+    public float KnockbackLength = .5f;
+    private float knockbackCounter;
+    public Vector2 KnockbackPower;
+
+    public GameObject[] PlayerPieces;
+
     private void Awake()
     {
         Instance = this;
@@ -40,35 +47,65 @@ public class PlayerController : MonoBehaviour
 
     void Movement()
     {
-        float yStore = moveDirection.y;
-        //moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
-        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
-        moveDirection.Normalize();
-        moveDirection *= MovementSpeed;
-        moveDirection.y = yStore;
-
-        if (CharContr.isGrounded)
+        if (!isKnockedOut)
         {
-            //Prevents Player for falling like crazy
-            moveDirection.y = 0f;
+            float yStore = moveDirection.y;
+            //moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) +
+                            (transform.right * Input.GetAxisRaw("Horizontal"));
+            moveDirection.Normalize();
+            moveDirection *= MovementSpeed;
+            moveDirection.y = yStore;
 
-            if (Input.GetButtonDown("Jump"))
+            if (CharContr.isGrounded)
             {
-                moveDirection.y = JumpForce;
+                //Prevents Player for falling like crazy
+                moveDirection.y = 0f;
+
+                if (Input.GetButtonDown("Jump"))
+                {
+                    moveDirection.y = JumpForce;
+                }
+            }
+
+            moveDirection.y += Physics.gravity.y * Time.deltaTime * GravityScale;
+            CharContr.Move(moveDirection * Time.deltaTime);
+
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                transform.rotation = Quaternion.Euler(0f, TheCamera.transform.rotation.eulerAngles.y, 0f);
+                Quaternion newRot = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+                PlayerModel.transform.rotation = Quaternion.Slerp(PlayerModel.transform.rotation, newRot,
+                    RotationSpeed * Time.deltaTime);
             }
         }
 
-        moveDirection.y += Physics.gravity.y * Time.deltaTime * GravityScale;
-        CharContr.Move(moveDirection * Time.deltaTime);
-
-        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        if (isKnockedOut)
         {
-            transform.rotation = Quaternion.Euler(0f, TheCamera.transform.rotation.eulerAngles.y, 0f);
-            Quaternion newRot = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
-            PlayerModel.transform.rotation = Quaternion.Slerp(PlayerModel.transform.rotation, newRot, RotationSpeed * Time.deltaTime);
+            knockbackCounter -= Time.deltaTime;
+            
+            float yStore = moveDirection.y;
+            moveDirection = (transform.forward * -KnockbackPower.x);
+            moveDirection.y = yStore;
+            if (CharContr.isGrounded)
+            {
+                moveDirection.y = 0;
+            }
+            moveDirection.y += Physics.gravity.y * Time.deltaTime * GravityScale;
+            CharContr.Move(moveDirection * Time.deltaTime);
+            
+            if(knockbackCounter <= 0)
+                isKnockedOut = false;
         }
-
         TheAnimator.SetFloat("Speed", Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
         TheAnimator.SetBool("Grounded", CharContr.isGrounded);
+    }
+
+    public void Knockback()
+    {
+        isKnockedOut = true;
+        knockbackCounter = KnockbackLength;
+        moveDirection.y = KnockbackPower.y;
+        CharContr.Move(moveDirection * Time.deltaTime);
     }
 }
