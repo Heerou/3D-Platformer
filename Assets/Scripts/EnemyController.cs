4316ea79776e670c3c2a13dc66be7b881732641b
Julio Alvarez Pimiento <jalvarezpimiento@gmail.com>
2023-09-09 14:14:59 -0500

    Enemy can move between points

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour
+{
+    public Transform[] PatrolPoints;
+    public int CurrentPatrolPoint;
+    public NavMeshAgent Agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetAgentDestination();
+    }
+
+    void SetAgentDestination()
+    {
+        Agent.SetDestination(PatrolPoints[CurrentPatrolPoint].position);
+        if (Agent.remainingDistance <= .2f)
+        {
+            CurrentPatrolPoint++;
+            if (CurrentPatrolPoint >= PatrolPoints.Length)
+            {
+                CurrentPatrolPoint = 0;
+            }
+            Agent.SetDestination(PatrolPoints[CurrentPatrolPoint].position);
+        }
+    }
+}
